8d49dd6863439f52496df18046ac0a265653d223
Amir Azuz <natural6t@gmail.com>
2025-12-26 22:19:11 +0100

    fix: load env before creating engine

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,13 @@
+# TODO: Delete in Production. DEV Only
 from dotenv import load_dotenv
+load_dotenv()
+
 from fastapi import FastAPI
 
 from app.api.exception_handler import register_exception_handlers
 from app.api.health import router as health_router
 from app.api.roadmaps import router as roadmaps_router
 from app.core.settings import API_PREFIX
-
-# TODO: Delete in Production. DEV Only
-load_dotenv()
 
 app = FastAPI()
 
